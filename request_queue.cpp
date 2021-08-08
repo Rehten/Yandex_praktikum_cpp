88ff3a299486a68eebaaa6fbe8a9afd5e9f6823a
Paumpeero <Paumpeero@rambler.ru>
2021-08-08 13:34:26 +0300

    sprint 4 - разделение поисковой системы с очередью запросов на файлы(вынесены функции работы со строками

--- request_queue.cpp
+++ request_queue.cpp
@@ -1,1 +1,61 @@
-//Вставьте сюда своё решение из урока «‎Очередь запросов».‎
+#pragma once
+
+#include <algorithm>
+
+#include "request_queue.h"
+
+using std::vector;
+using std::string;
+
+RequestQueue::RequestQueue(const SearchServer &search_server) : search_server_(search_server)
+{}
+
+template<typename DocumentPredicate>
+vector<Document> RequestQueue::AddFindRequest(const string &raw_query, DocumentPredicate document_predicate)
+{
+  vector<Document> response = search_server_.FindTopDocuments(raw_query, document_predicate);
+
+  UpdateRequests({response.size()});
+
+  return response;
+}
+
+vector<Document> RequestQueue::AddFindRequest(const string &raw_query, DocumentStatus status)
+{
+  vector<Document> response = search_server_.FindTopDocuments(raw_query, status);
+
+  UpdateRequests({response.size()});
+
+  return response;
+}
+
+vector<Document> RequestQueue::AddFindRequest(const string &raw_query)
+{
+  vector<Document> response = search_server_.FindTopDocuments(raw_query);
+
+  UpdateRequests({response.size()});
+
+  return response;
+}
+
+int RequestQueue::GetNoResultRequests() const
+{
+  return count_if(requests_.rbegin(), requests_.rend(), [](QueryResult rslt) -> bool {
+    return !rslt.founded;
+  });
+}
+
+const int RequestQueue::sec_in_day_ = 1440;
+
+void RequestQueue::UpdateRequests(QueryResult queryResult)
+{
+  requests_.push_back(queryResult);
+
+  if (requests_.size() > sec_in_day_)
+  {
+    requests_.pop_front();
+  }
+}
+
+RequestQueue::QueryResult::QueryResult(size_t fndd) : founded(fndd)
+{}
--- search_server.h
+++ search_server.h
@@ -1,1 +1,80 @@
-//Вставьте сюда своё решение из урока «‎Очередь запросов».‎
+#pragma once
+
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+
+#include "read_input_functions.h"
+#include "string_processing.h"
+#include "paginator.h"
+
+class SearchServer
+{
+public:
+  template<typename StringContainer>
+  explicit SearchServer(const StringContainer &stop_words);
+
+  explicit SearchServer(const std::string &stop_words_text);
+
+  void
+  AddDocument(int document_id, const std::string &document, DocumentStatus status, const std::vector<int> &ratings);
+
+  template<typename DocumentPredicate>
+  std::vector<Document> FindTopDocuments(const std::string &raw_query, DocumentPredicate document_predicate) const;
+
+  std::vector<Document> FindTopDocuments(const std::string &raw_query, DocumentStatus status) const;
+
+  std::vector<Document> FindTopDocuments(const std::string &raw_query) const;
+
+  int GetDocumentCount() const;
+
+  int GetDocumentId(int index) const;
+
+  std::tuple<std::vector<std::string>, DocumentStatus>
+  MatchDocument(const std::string &raw_query, int document_id) const;
+
+private:
+  struct DocumentData
+  {
+    int rating;
+    DocumentStatus status;
+  };
+
+  static const int MaxResultDocumentCount;
+
+  const std::set<std::string> stop_words_;
+  std::map<std::string, std::map<int, double>> word_to_document_freqs_;
+  std::map<int, DocumentData> documents_;
+  std::vector<int> document_ids_;
+
+  static bool IsValidWord(const std::string &word);
+
+  bool IsStopWord(const std::string &word) const;
+
+  std::vector<std::string> SplitIntoWordsNoStop(const std::string &text) const;
+
+  static int ComputeAverageRating(const std::vector<int> &ratings);
+
+  struct QueryWord
+  {
+    std::string data;
+    bool is_minus;
+    bool is_stop;
+  };
+
+  struct Query
+  {
+    std::set<std::string> plus_words;
+    std::set<std::string> minus_words;
+  };
+
+  QueryWord ParseQueryWord(const std::string &text) const;
+
+  Query ParseQuery(const std::string &text) const;
+
+  double ComputeWordInverseDocumentFreq(const std::string &word) const;
+
+  template<typename DocumentPredicate>
+  std::vector<Document> FindAllDocuments(const Query &query, DocumentPredicate document_predicate) const;
+};
