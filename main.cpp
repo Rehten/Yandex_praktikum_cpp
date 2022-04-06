c1b190c89df2a0a6f796d2d2b2b98d48b1f2aa16
Anton Sysoev <sisoev.a@n-t.io>
2022-04-07 00:10:21 +0300

    Спринт 10 - unique_ptr - прошел один тест

--- main.cpp
+++ main.cpp
@@ -1,97 +1,121 @@
-#include <iostream>
 #include <cassert>
+#include <iostream>
+#include <memory>
 
-using namespace std;
+template<typename T>
+struct TreeNode;
 
-template <class T>
-struct TreeNode {
-    T value;
-    TreeNode* parent = nullptr;
-    TreeNode* left = nullptr;
-    TreeNode* right = nullptr;
+template<typename T>
+using TreeNodePtr = std::unique_ptr<TreeNode<T>>;
+
+template<typename T>
+struct TreeNode
+{
+  TreeNode(T val, TreeNodePtr<T> &&left, TreeNodePtr<T> &&right)
+    : value(std::move(val)), left(std::move(left)), right(std::move(right))
+  {
+  }
+
+  T value;
+  TreeNodePtr<T> left;
+  TreeNodePtr<T> right;
+  TreeNode *parent = nullptr;
 };
 
-template <class T>
-void DeleteTree(TreeNode<T>* node) {
-    if (!node) {
-        return;
-    }
-    DeleteTree(node->left);
-    DeleteTree(node->right);
-    delete node;
+template<typename T>
+bool CheckTreeProperty(const TreeNode<T> *node) noexcept
+{
+  if (!node->left.get() || !node->right.get())
+  {
+    return (
+      (node->left.get() == nullptr || node->left->value <= node->value)
+      &&
+      (node->right.get() == nullptr || node->right->value >= node->value)
+    );
+  }
+
+  bool is_left_and_right_are_balanced(
+    (node->left.get() == nullptr || node->left->value <= node->value)
+    &&
+    (node->right.get() == nullptr || node->right->value >= node->value)
+  );
+
+  if (!is_left_and_right_are_balanced)
+  {
+    return false;
+  }
+
+  return CheckTreeProperty(node->left.get()) && CheckTreeProperty(node->right.get());
 }
 
-template <class T>
-void PrintTree(const TreeNode<T>* root, ostream& out = cout) {
-    out << " ( "s;
-    out << root->value;
-    if (root->left || root->right) {
-        if (root->left) {
-            PrintTree(root->left, out);
-        } else {
-            out << "*"s;
-        }
-        if (root->right) {
-            PrintTree(root->right, out);
-        } else {
-            out << "*"s;
-        }
-    }
-    out << " ) "s;
+template<typename T>
+TreeNode<T> *begin(TreeNode<T> *node) noexcept
+{
+  if (node->left.get() == nullptr)
+  {
+    return nullptr;
+  }
+
+  while (node->left.get())
+  {
+    node = node->left.get();
+  }
+
+  return node;
 }
 
-template <class T>
-ostream& operator << (ostream& out, const TreeNode<T>* node) {
-    PrintTree(node, out);
-    return out;
-}
+template<typename T>
+TreeNode<T> *next(TreeNode<T> *node) noexcept
+{
+  if (node->right.get() == nullptr && node->parent->left.get() != node)
+  {
+    return nullptr;
+  }
 
-template <class T>
-TreeNode<T>* begin(TreeNode<T>* node) {
-    while (node->left)
+  if (node->parent->left.get() == node)
+  {
+    return node->parent;
+  }
+
+  if (node->right.get()->right.get() == nullptr)
+  {
+    TreeNode<T> *node_copy = node->right.get();
+
+    while (node_copy->left.get())
     {
-        node = node->left;
+      node_copy = node_copy->left.get();
     }
 
-    return node;
-}
+    return node_copy;
+  }
 
-template <class T>
-TreeNode<T>* next(TreeNode<T>* node) {
-    if (node->right) return begin(node->right);
-    if (node->parent->left == node) return node->parent;
-    if (node->parent->right == node) return node->parent->parent;
-    if (!node->right) return node->parent;
-
-    assert(false);
+  throw std::runtime_error("Errored Algorithm");
 }
 
 // функция создаёт новый узел с заданным значением и потомками
-TreeNode<int>* N(int val, TreeNode<int>* left = nullptr, TreeNode<int>* right = nullptr) {
-    auto res = new TreeNode<int>{val, nullptr, left, right};
-    if (left) {
-        left->parent = res;
-    }
-    if (right) {
-        right->parent = res;
-    }
-
-    return res;
+TreeNodePtr<int> N(int val, TreeNodePtr<int> &&left = {}, TreeNodePtr<int> &&right = {})
+{
+  return std::make_unique<TreeNode<int>>(val, std::move(left), std::move(right));
 }
 
-int main() {
-    using T = TreeNode<int>;
+int main()
+{
+  using namespace std;
+  using T = TreeNode<int>;
+  auto root1 = N(6, N(4, N(3), N(5)), N(7));
+  assert(CheckTreeProperty(root1.get()));
 
-    T* root = N(6, N(4, N(3), N(5)), N(8, N(7)));
-    cout << root << endl;
+  T *iter = begin(root1.get());
+  while (iter)
+  {
+    cout << iter->value << " "s;
+    iter = next(iter);
+  }
+  cout << endl;
 
-    T* iter = begin(root);
+  auto root2 = N(6, N(4, N(3), N(5)), N(7, N(8)));
+  assert(!CheckTreeProperty(root2.get()));
 
-    while (iter) {
-        cout << iter->value << " "s;
-        iter = next(iter);
-    }
-    cout << endl;
-
-    DeleteTree(root);
+  // Функция DeleteTree не нужна. Узлы дерева будут рекурсивно удалены
+  // благодаря деструкторам unique_ptr
 }
